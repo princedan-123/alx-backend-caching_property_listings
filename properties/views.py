537db568d61f13317f283b4e55b242938fe7ff17
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-08-26 17:00:56 +0100

    implemented per page caching

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.core.cache import cache
+from django.http import JsonResponse
+from django.core import serializers
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+@cache_page(60 * 15)
+def property_list(request):
+    queryset = Property.objects.all()
+    data = serializers.serializer('json', queryset)
+    return JsonResponse(data, safe=False)
