ab87270eaecab0a785fcec33ff7743c9b9c08151
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-08-26 06:12:42 +0100

    removed space from models

--- properties/models.py
+++ properties/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Property (models.Model):
+class Property(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
