9b06544fe63cdd34e0ada337bf7b32ae7add0ab7
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-08-27 12:17:24 +0100

    used signal to remote stale data from cache

--- alx_backend_caching_property_listings/properties/apps.py
+++ alx_backend_caching_property_listings/properties/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        from .signals import delete_cache_on_delete
+        from .signals import delete_cache_on_save
