5e715d757c8425b1b016abfb2a7742ec94c4affe
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-08-27 20:01:42 +0100

    calculated metrics

--- properties/utils.py
+++ properties/utils.py
@@ -1,5 +1,6 @@
 from django.core.cache import cache
 from .models import Property
+from django_redis import get_redis_connection
 
 def get_all_properties():
     """A utitlity for low level caching."""
@@ -10,3 +11,10 @@ def get_all_properties():
         cache.set('all_properties', queryset, 3600)
         return queryset
     return queryset
+
+def get_redis_cache_metrics():
+    redis = get_redis_connection('default')
+    hits = redis.info.get('keyspace_hits', 0)
+    misses = redis.info.get('keyspace_misses', 0)
+    metrics =  (hits / (hits + misses))
+    return {"metrics": metrics}
